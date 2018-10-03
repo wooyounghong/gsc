1effebfac4977605bac4b053e6d9fd75b3ece520
Wayne Hong <woo.young.hong@hotmail.com>
2018-10-03 17:48:49 +0900

    deployed successfully via MS Azure with static html file rendering properly

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  console.log('response is sent!');
-  res.send('hello world!');
+// middlewares:
+app.use(express.static(`${__dirname}/../client/dist`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(`${__dirname}/../client/dist/index.html`));
 });
+
 app.listen(PORT, () => {
   console.log('GSC port is listening at ', PORT);
 });
