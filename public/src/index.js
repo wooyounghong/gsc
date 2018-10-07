bde183b90dc867ac8deb1597524f8036df2eb2c9
Wayne Hong <woo.young.hong@hotmail.com>
2018-10-07 14:10:11 +0900

    added jquery and scrollTop method. need to check on deployment for test

--- public/src/index.js
+++ public/src/index.js
@@ -1,1 +1,21 @@
-console.log('hi');
+// console.log('hi');
+
+// routing to the div element;
+// without jQuery;
+
+// const clickScrollTo = id => {
+//   const element = document.getElementById(id);
+//   console.log(element);
+//   element.scrollIntoView({
+//     behavior: 'smooth',
+//   });
+// };
+
+// with jquery;
+// window.scrollTo(0, 0);
+
+$(document).ready(function() {
+  // create a scroll to top when the page renders;
+  // console.log('this', this);
+  $(window).scrollTop();
+});
