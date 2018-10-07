1e494faa60d765fa1ae501b5d186ee9f18130675
Wayne Hong <woo.young.hong@hotmail.com>
2018-10-07 14:43:22 +0900

    added README, created scroll event to correct divs, testing with deployment

---
+++ README.md
@@ -0,0 +1,1 @@
+GoSmartCloud, LLC is a start-up cloud consulting company that consults with other companies to help them set up, watch, utilize Cloud platform such as MS Azure, Oracle, and AWS.
--- public/src/index.js
+++ public/src/index.js
@@ -1,4 +1,4 @@
-// console.log('hi');
+console.log('hi');
 
 // routing to the div element;
 // without jQuery;
@@ -15,7 +15,19 @@
 // window.scrollTo(0, 0);
 
 $(document).ready(function() {
-  // create a scroll to top when the page renders;
-  // console.log('this', this);
-  $(window).scrollTop();
+  // scrolling to top;
+  $('html, body').animate({ scrollTop: 0 }, 700);
+
+  // scroll to services container;
+  $('#services-navbar').click(function() {
+    $(window).scrollTop($('#services-container').offset().top);
+  });
+  // scroll to about container;
+  $('#about-navbar').click(function() {
+    $(window).scrollTop($('#about-container').offset().top);
+  });
+  $('#contact-navbar').click(function() {
+    $(window).scrollTop($('#contact-container').offset().top);
+  });
+  // scroll to contact container;
 });
